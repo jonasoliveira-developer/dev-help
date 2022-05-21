a5a712a2746725f30a4fbfa06ef7da1fad2bd0d9
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-05-21 11:29:33 -0300

    projeto iniciado

---
+++ src/main/java/com/oliveira/jonas/devhelp/DevhelpApplication.java
@@ -0,0 +1,13 @@
+package com.oliveira.jonas.devhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevhelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevhelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonas/oliveira/devhelp/DevhelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonas.oliveira.devhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
