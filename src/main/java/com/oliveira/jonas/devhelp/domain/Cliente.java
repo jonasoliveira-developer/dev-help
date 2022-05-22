c1ab7cbb5737a8e8bc83cbdfc96e72f9c7c97dab
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-05-22 01:44:02 -0300

    criacao e Anotações do dominio

--- src/main/java/com/oliveira/jonas/devhelp/domain/Cliente.java
+++ src/main/java/com/oliveira/jonas/devhelp/domain/Cliente.java
@@ -1,5 +1,34 @@
 package com.oliveira.jonas.devhelp.domain;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamados> chamados = new ArrayList<>();
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamados> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamados> chamados) {
+		this.chamados = chamados;
+	}
+
+
+	
 }
--- src/main/java/com/oliveira/jonas/devhelp/domain/Tecnico.java
+++ src/main/java/com/oliveira/jonas/devhelp/domain/Tecnico.java
@@ -1,5 +1,36 @@
 package com.oliveira.jonas.devhelp.domain;
 
-public class Tecnico {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+
+@Entity
+public class Tecnico extends Pessoa {
+	
+	
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamados> chamados = new ArrayList<>();
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamados> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamados> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
 }
