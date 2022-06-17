62ad178b4d2b4d5586f86a166ba463d131843c85
Jonas Oliveira <jonasoliveira.webdeveloper@gmail.com>
2022-06-17 19:09:02 -0300

    Método FindAll implementado para Chamados

--- src/main/java/com/oliveira/jonas/devhelp/services/ChamadoService.java
+++ src/main/java/com/oliveira/jonas/devhelp/services/ChamadoService.java
@@ -1,6 +1,7 @@
 package com.oliveira.jonas.devhelp.services;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.oliveira.jonas.devhelp.domain.Chamados;
 import com.oliveira.jonas.devhelp.repositories.ChamadoRepository;
 import com.oliveira.jonas.devhelp.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class ChamadoService {
@@ -17,6 +20,12 @@ public class ChamadoService {
 	
 	public Chamados findById(Integer id) {
 		Optional<Chamados> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!" + id));
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! " + id));
 	}
+
+	public List<Chamados> findAll() {
+		return repository.findAll();
+	}
+	
+	
 }
